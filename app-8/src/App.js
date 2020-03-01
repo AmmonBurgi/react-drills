621dbba087cc37ff3d88b4819b554bfea5d6cffb
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-03-01 15:56:55 -0700

    completed

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import './App.css';
+import axios from 'axios'
+
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      skyWalker: ''
+    }
+  }
+  componentDidMount(){
+    axios.get('https://swapi.co/api/people/1')
+    .then(response => {
+      this.setState({
+        skyWalker: response.data
+      })
+    })
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Name: {this.state.skyWalker.name}</h2>
+    <h2>Height: {this.state.skyWalker.height}</h2>
+    <h2>Gender: {this.state.skyWalker.gender}</h2>
     </div>
   );
+  }
 }
 
 export default App;
